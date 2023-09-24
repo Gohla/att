b66e2a87ac205605b3d6543137de1c2c4d7be83e
Gabriel Konat <gabrielkonat@gmail.com>
2023-09-24 16:18:03 +0200

    Cleanup.

--- att/src/widget/dark_light_toggle.rs
+++ att/src/widget/dark_light_toggle.rs
@@ -1,15 +1,15 @@
 use iced::alignment::Horizontal;
 use iced::Element;
-use iced::widget::{Button, Text};
 
-use crate::widget::{ButtonEx, ICON_FONT};
+use crate::widget::builder::WidgetBuilder;
+use crate::widget::ICON_FONT;
 
 pub fn light_dark_toggle<'a, M: 'a>(dark_mode_enabled: bool, on_press: impl Fn() -> M + 'a) -> Element<'a, M> {
   let icon = if dark_mode_enabled { "\u{f5a2}" } else { "\u{f496}" };
-  let text = Text::new(icon)
-    .font(ICON_FONT)
-    .horizontal_alignment(Horizontal::Center)
-    .width(20);
-  Button::new(text)
-    .on_press_into_element(on_press)
+  let text = WidgetBuilder::default()
+    .text(icon).font(ICON_FONT).horizontal_alignment(Horizontal::Center).width(20).add()
+    .take();
+  WidgetBuilder::default()
+    .button(text).add(on_press)
+    .take()
 }
--- att/src/widget/mod.rs
+++ att/src/widget/mod.rs
@@ -1,40 +1,9 @@
-use iced::{Command, Element, Font};
-use iced::advanced::{Renderer, Widget};
-use iced::widget::button::{self, Button};
+use iced::{Command, Font};
 use iced_futures::MaybeSend;
 
 pub mod modal;
 pub mod dark_light_toggle;
 pub mod builder;
-
-/// Widget extensions
-pub trait WidgetExt<'a, M, R> {
-  fn into_element(self) -> Element<'a, M, R>;
-  fn map_into_element<MM: 'a, F: Fn(M) -> MM + 'a>(self, f: F) -> Element<'a, MM, R>;
-}
-impl<'a, M: 'a, R: Renderer + 'a, W: Widget<M, R> + 'a> WidgetExt<'a, M, R> for W {
-  #[inline]
-  fn into_element(self) -> Element<'a, M, R> {
-    Element::new(self)
-  }
-  #[inline]
-  fn map_into_element<MM: 'a, F: Fn(M) -> MM + 'a>(self, f: F) -> Element<'a, MM, R> {
-    self.into_element().map(f)
-  }
-}
-
-/// Button widget extensions
-pub trait ButtonEx<'a, R> {
-  fn on_press_into_element<M: 'a, F: Fn() -> M + 'a>(self, f: F) -> Element<'a, M, R>;
-}
-impl<'a, R> ButtonEx<'a, R> for Button<'a, (), R> where
-  R: Renderer + 'a,
-  R::Theme: button::StyleSheet,
-{
-  fn on_press_into_element<M: 'a, F: Fn() -> M + 'a>(self, f: F) -> Element<'a, M, R> {
-    self.on_press(()).map_into_element(move |_| f())
-  }
-}
 
 /// [Bootstrap icon](https://icons.getbootstrap.com/) font. Only available after [`load_icon_font_command`] completes.
 pub const ICON_FONT: Font = Font::with_name("bootstrap-icons");
