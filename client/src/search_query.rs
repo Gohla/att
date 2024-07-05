aaaafc05b519503886b7502d7053d96079b48de6
Gabriel Konat <gabrielkonat@gmail.com>
2024-07-05 09:52:22 +0200

    Simplify search_query.

--- client/src/search_query.rs
+++ client/src/search_query.rs
@@ -1,39 +1,30 @@
-use std::fmt::{Debug, Display};
+use std::fmt::Debug;
 use std::future::Future;
 use std::time::Duration;
 
 use futures::FutureExt;
-use tracing::{debug, error};
 
 use att_core::query::{Query, QueryMessage};
 use att_core::util::maybe_send::{MaybeSend, MaybeSendFuture};
 use att_core::util::time::{Instant, sleep};
 
 #[derive(Debug)]
-pub struct SearchQuery<T, Q, Fn> {
-  create_future: Fn,
-  default_query: Q,
-
+pub struct SearchQuery<Q, F> {
   query: Q,
   wait_until: Option<Instant>,
-  data: Vec<T>,
+  create_future: F,
 }
-impl<T, Q, E, Fut, F> SearchQuery<T, Q, F> where
-  T: 'static,
+impl<Q, F, R, Fut> SearchQuery<Q, F> where
   Q: Query + Clone + 'static,
-  E: Display + Debug + 'static,
-  Fut: Future<Output=Result<Vec<T>, E>> + Send + 'static,
-  F: Fn(Q) -> Fut + 'static
+  F: Fn(Q) -> Fut + 'static,
+  R: 'static,
+  Fut: Future<Output=R> + Send + 'static,
 {
-  pub fn new(default_query: Q, create_future: F) -> Self {
-    let query = default_query.clone();
+  pub fn new(query: Q, create_future: F) -> Self {
     Self {
-      create_future,
-      default_query,
-
       query,
       wait_until: None,
-      data: Vec::default(),
+      create_future,
     }
   }
 
@@ -46,22 +37,13 @@ impl<T, Q, E, Fut, F> SearchQuery<T, Q, F> where
   #[inline]
   pub fn query_mut(&mut self) -> &mut Q { &mut self.query }
 
-  /// Returns the data.
-  #[inline]
-  pub fn data(&self) -> &[T] { &self.data }
 
-  /// Returns the mutable data.
-  #[inline]
-  pub fn data_mut(&mut self) -> &mut Vec<T> { &mut self.data }
-
-
-  /// Update the query from `message`, possibly returning a future producing a [response](WaitCleared) that
-  /// must be [processed](Self::process_wait_cleared).
+  /// Update the query from `message`, returning `Some(future)` producing a [response](WaitCleared) that
+  /// must be [processed](Self::process_wait_cleared) if the query is not empty. Returns `None` if the query is empty.
   pub fn update_query(&mut self, message: QueryMessage) -> Option<impl Future<Output=WaitCleared>> {
     message.update_query(&mut self.query);
     if self.query.is_empty() {
       self.wait_until = None;
-      self.data.clear();
       None
     } else {
       let wait_duration = Duration::from_millis(300);
@@ -77,7 +59,7 @@ impl<T, Q, E, Fut, F> SearchQuery<T, Q, F> where
 
   /// Process a [wait cleared response](WaitCleared), possibly returning a future producing a [response](QueryResult)
   /// that must be [processed](Self::process_result).
-  pub fn process_wait_cleared(&self, _response: WaitCleared) -> Option<impl Future<Output=QueryResult<T, E>>> {
+  pub fn process_wait_cleared(&self, _response: WaitCleared) -> Option<impl Future<Output=QueryResult<R>>> {
     self.should_send_query().then(|| self.send_current_query())
   }
 
@@ -90,27 +72,22 @@ impl<T, Q, E, Fut, F> SearchQuery<T, Q, F> where
 
   /// Sends the current query, returning a future producing a [response](QueryResult) that must be
   /// [processed](Self::process_result).
-  pub fn send_current_query(&self) -> impl Future<Output=QueryResult<T, E>> {
+  pub fn send_current_query(&self) -> impl Future<Output=QueryResult<R>> {
     let future = (self.create_future)(self.query.clone());
     async move {
       QueryResult { result: future.await }
     }
   }
 
-  /// Processes a [query result response](QueryResult), updating the data.
-  pub fn process_result(&mut self, response: QueryResult<T, E>) -> Result<(), E> {
-    let data = response.result
-      .inspect_err(|cause| error!(?cause, "failed to query: {cause}"))?;
-    debug!("query resulted in {} data elements", data.len());
-    self.data = data;
-
-    Ok(())
+  /// Processes a [query result response](QueryResult), returning the result.
+  pub fn process_result(&mut self, response: QueryResult<R>) -> R {
+    response.result
   }
 
 
   /// Send a [request](QueryRequest), possibly returning a future producing a [response](QueryResponse)
   /// that must be [processed](Self::process).
-  pub fn send(&mut self, request: QueryRequest) -> Option<impl Future<Output=QueryResponse<T, E>> + MaybeSend> {
+  pub fn send(&mut self, request: QueryRequest) -> Option<impl Future<Output=QueryResponse<R>> + MaybeSend> {
     use QueryRequest::*;
     use QueryResponse::*;
     match request {
@@ -120,21 +97,13 @@ impl<T, Q, E, Fut, F> SearchQuery<T, Q, F> where
   }
 
   /// Process a [response](QueryResponse), possibly returning a request that must be [sent](Self::send_current_query).
-  pub fn process(&mut self, response: QueryResponse<T, E>) -> Option<QueryRequest> {
+  pub fn process(&mut self, response: QueryResponse<R>) -> Option<QueryRequest> {
     use QueryResponse::*;
     match response {
       WaitCleared(_) => return self.should_send_query().then_some(QueryRequest::SendCurrentQuery),
       QueryResult(r) => { let _ = self.process_result(r); },
     }
     None
-  }
-
-
-  /// Clears the query and data, and cancels ongoing queries.
-  pub fn clear(&mut self) {
-    self.query = self.default_query.clone();
-    self.wait_until = None;
-    self.data.clear();
   }
 }
 
@@ -144,8 +113,8 @@ pub struct WaitCleared;
 
 /// Data from query response.
 #[derive(Debug)]
-pub struct QueryResult<T, E> {
-  result: Result<Vec<T>, E>,
+pub struct QueryResult<R> {
+  result: R,
 }
 
 /// Search crate requests in message form.
@@ -157,15 +126,15 @@ pub enum QueryRequest {
 
 /// Search crate responses in message form.
 #[derive(Debug)]
-pub enum QueryResponse<T, E> {
+pub enum QueryResponse<R> {
   WaitCleared(WaitCleared),
-  QueryResult(QueryResult<T, E>),
+  QueryResult(QueryResult<R>),
 }
-impl<T, E> From<WaitCleared> for QueryResponse<T, E> {
+impl<R> From<WaitCleared> for QueryResponse<R> {
   #[inline]
   fn from(r: WaitCleared) -> Self { Self::WaitCleared(r) }
 }
-impl<T, E> From<QueryResult<T, E>> for QueryResponse<T, E> {
+impl<R> From<QueryResult<R>> for QueryResponse<R> {
   #[inline]
-  fn from(r: QueryResult<T, E>) -> Self { Self::QueryResult(r) }
+  fn from(r: QueryResult<R>) -> Self { Self::QueryResult(r) }
 }
